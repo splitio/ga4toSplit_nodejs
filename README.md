96bd3c1108a54acd330255a3511db3c9d13c8ac7
David Martin <david.martin@split.io>
2023-04-20 13:30:21 -0500

    init

---
+++ README.md
@@ -0,0 +1,61 @@
+# GA4 to Split - node.js
+
+This is an unfinished, but working integration of GA4 event traffic to Split events.
+
+## To install 
+
+Clone the repository and run 'npm install' to download dependencies.
+
+ - Customer should use GA4 (gtag) in HTML/JS or similar
+ - Verify that you can see the event traffic in Google
+ - Link BigQuery to your GA project
+
+Choose "transactionally" over daily for best results.
+
+ - Run a sample query in BigQuery console
+
+```
+  select 
+    ep.key as eventTypeId,
+    ep.value.string_value as title,
+    event_timestamp as ts,
+    user_id as trafficKey,
+    device.category as deviceCategory,
+    device.mobile_brand_name as mobileBrandName,
+    device.mobile_model_name as mobileModelName,
+    device.operating_system as os,
+    device.operating_system_version as osVersion,
+    geo.continent as geoContinent,
+    geo.country as geoCountry,
+    geo.region as geoRegion,
+    geo.city as geoCity,
+    geo.sub_continent as geoSubcontinent,
+    geo.metro as metro
+  from ${table},
+  unnest(event_params) as ep
+  where user_id is not NULL
+```
+
+Where table should look like this (note the intraday timestamp):  
+
+```
+  const table = '\`split-and-ga4.analytics_369415822.events_intraday_20230420\`';
+```
+
+If you get back good looking results, you are ready for the next step.
+
+ - Manually edit the right table name into index.js
+ - Create a SPLIT_API_KEY file and copy-and-paste your key, with no spaces or new lines, into your file
+ - Create a new Service Account in Google and download a JSON key.
+ - Give the Service Account permissions to your dataset (use the client_email from your JSON key).
+
+```
+node index.js
+```
+
+## What next?
+
+The script is unfinished.  It needs to discover the table by date automatically. 
+
+Also, the events should be batched to Split.  Right now they're sent individually.
+ 
---
+++ index.js
@@ -0,0 +1,95 @@
+const { BigQuery } = require('@google-cloud/bigquery');
+const fs = require('fs');
+const axios = require('axios');
+
+async function runQuery() {
+  // Read the JSON key file
+  const jsonKeyFilePath = 'split-and-ga4-5a6eb1cc47f0.json';
+  const jsonKeyFile = fs.readFileSync(jsonKeyFilePath, 'utf8');
+  const credentials = JSON.parse(jsonKeyFile);
+
+  // Create a client using the credentials object
+  const bigqueryClient = new BigQuery({
+    projectId: credentials.project_id,
+    credentials: credentials,
+  });
+
+  // Set your dataset ID
+  const datasetId = 'analytics_369415822';
+
+  // Define your SQL query
+  const table = '\`split-and-ga4.analytics_369415822.events_intraday_20230420\`';
+
+  const sqlQuery = `
+  select 
+    ep.key as eventTypeId,
+    ep.value.string_value as title,
+    event_timestamp as ts,
+    user_id as trafficKey,
+    device.category as deviceCategory,
+    device.mobile_brand_name as mobileBrandName,
+    device.mobile_model_name as mobileModelName,
+    device.operating_system as os,
+    device.operating_system_version as osVersion,
+    geo.continent as geoContinent,
+    geo.country as geoCountry,
+    geo.region as geoRegion,
+    geo.city as geoCity,
+    geo.sub_continent as geoSubcontinent,
+    geo.metro as metro
+  from ${table},
+  unnest(event_params) as ep
+  where user_id is not NULL`;
+  
+  console.log(sqlQuery);
+
+  // Run the query as a job
+  const [job] = await bigqueryClient.createQueryJob({
+    query: sqlQuery
+  });
+
+  // Wait for the query to finish
+  const [rows] = await job.getQueryResults();
+
+  // rows.forEach(row => console.log(row));
+  rows.forEach(async row => {
+    console.log(row);
+
+    const props = {
+      'event_params.value.string_value': row.title ? row.title : '',
+      'device.category': row.deviceCategory,
+      'device.mobile_brand_name': row.mobileBrandName,
+      'device.mobile_model_name': row.mobileModelName,
+      'device.operating_system': row.os,
+      'device.operating_system_version': row.osVersion,
+      'geo.continent': row.geoContinent,
+      'geo.country': row.geoCountry,
+      'geo.region': row.geoRegion,
+      'geo.city': row.geoCity,
+      'geo.sub_continent': row.geoSubcontinent,
+      'geo.metro': row.metro
+    }
+
+    let data = [{
+      eventTypeId: row.eventTypeId,
+      trafficTypeName: 'user', // should this be more flexible?
+      key: row.trafficKey,
+      timestamp: row.ts,
+      properties: props,
+      source: 'BiqQuery'
+    }];
+
+    const splitApiKey = fs.readFileSync('SPLIT_API_KEY', 'utf8').trim();
+    console.log(data);
+    await axios.post('https://events.split.io/api/events/bulk', data, 
+      { headers: {'Authorization': 'Bearer ' + splitApiKey }}) 
+    .then(function (response) {
+        console.log(response.status);
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+  });
+}
+
+runQuery().catch(console.error.response);
